52f82780983d5c35f24b5f35f2cdcff2dcf31c43
4Vegas <4vegasss@gmail.com>
2022-10-28 17:12:03 +0300

    third_commit

--- landingpage/settings.py
+++ landingpage/settings.py
@@ -15,10 +15,10 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
 # For debug version
-# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # For server version
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-9#wwo^&6#q+v5_4lh1!g%7!#4r+f_act$=%wq3xgid*zf1iy+s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1',]
+ALLOWED_HOSTS = ['http://sev.pythonanywhere.com/', '127.0.0.1',]
 
 
 # Application definition
